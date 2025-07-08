7ed1a2f5fac5f695305cffac32ffe59192029d8d
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-08 10:51:33 +0400

    Refactor Footer and Cta components to enhance layout and user interaction. Updated Footer with new branding, office information, and social links. Improved Cta section with contact options including phone and email links, and enhanced styling for better visual appeal.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,17 +2,44 @@ import Link from "next/link";
 
 import { IconArrowUp } from "@tabler/icons-react";
 
+import { Logo } from "@/assets/logo";
+
 export const Footer = () => {
   return (
-    <div className="text-primary-foreground/60 container flex items-center justify-between py-3">
-      <Link href="/privacy" className="text-primary-foreground/60">
-        Terms & Conditions
-      </Link>
+    <footer>
+      <div className="container grid grid-cols-3 gap-3 py-12">
+        <div>
+          <Link href="/">
+            <Logo />
+          </Link>
+          <h4 className="font-aloevera mt-8 text-2xl font-medium">
+            Head Office
+          </h4>
+          <p className="mt-3">
+            Piptan Consulting, World Trade Centre,
+            <br /> Office No. 2407, Level 24 <br />
+            Sheikh Zayed Road, Dubai, UAE
+          </p>
+          <h4 className="font-aloevera mt-8 text-2xl font-medium">
+            Follow us on
+          </h4>
+          <ul className="mt-3 flex items-center gap-4">
+            <li>I</li>
+            <li>F</li>
+            <li>Y</li>
+          </ul>
+        </div>
+      </div>
+      <div className="text-primary-foreground/60 border-muted-foreground/50 container flex items-center justify-between border-t py-3">
+        <Link href="/privacy" className="text-primary-foreground/60">
+          Terms & Conditions
+        </Link>
 
-      <Link href="#" className="flex items-center gap-2">
-        Scroll to top{" "}
-        <IconArrowUp className="text-primary-foreground/60 size-4 shrink-0" />
-      </Link>
-    </div>
+        <Link href="#" className="flex items-center gap-2">
+          Scroll to top{" "}
+          <IconArrowUp className="text-primary-foreground/60 size-4 shrink-0" />
+        </Link>
+      </div>
+    </footer>
   );
 };
--- src/components/sections/cta.tsx
+++ src/components/sections/cta.tsx
@@ -1,8 +1,44 @@
+import Link from "next/link";
+
+import { IconMail, IconPhone } from "@tabler/icons-react";
+
 export const Cta = () => {
   return (
     <section className="bg-primary">
-      <div className="container py-12">
-        <h3>Let&apos;s Grow your Business Together</h3>
+      <div className="container grid grid-cols-2 gap-6 py-12">
+        <h3 className="font-aloevera text-6xl font-medium">
+          Let&apos;s Grow your Business Together
+        </h3>
+        <div className="grid grid-cols-2 gap-6">
+          <Link
+            href="tel:+971987654321"
+            className="group bg-primary-foreground text-foreground flex items-center justify-center gap-3 rounded-md border border-white shadow-xl transition-shadow hover:shadow-lg"
+          >
+            <div className="bg-primary text-primary-foreground grid size-16 place-content-center rounded-full">
+              <IconPhone className="size-9 stroke-1" />
+            </div>
+            <div>
+              <h4 className="font-aloevera text-xl">Call us now!</h4>
+              <p className="group-hover:text-primary text-2xl font-semibold tracking-wide">
+                +971 54 454 5454
+              </p>
+            </div>
+          </Link>
+          <Link
+            href="mailto:info@piptan.ae"
+            className="group bg-primary-foreground text-foreground flex items-center justify-center gap-3 rounded-md border border-white shadow-xl transition-shadow hover:shadow-lg"
+          >
+            <div className="bg-primary text-primary-foreground grid size-16 place-content-center rounded-full">
+              <IconMail className="size-9 stroke-1" />
+            </div>
+            <div>
+              <h4 className="font-aloevera text-xl">Mail us anytime!</h4>
+              <p className="group-hover:text-primary text-2xl font-semibold tracking-wide">
+                consult@piptan.ae
+              </p>
+            </div>
+          </Link>
+        </div>
       </div>
     </section>
   );
